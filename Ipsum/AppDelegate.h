7d53fb89791cd0c06b0f3dea92b2f98dc36f4c94
Parker Melech <pzag2@aol.com>
2012-08-16 16:32:51 -0500

    Added in the Ipsum Project

---
+++ Ipsum/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ipsum
+//
+//  Created by Parker Melech on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
